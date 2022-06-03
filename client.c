d1ff8f70830a4fa3248d2311a569bab9d247b49e
Refrigerator <gngn15401@gmail.com>
2022-06-04 01:21:07 +0800

    remove file write code

--- client.c
+++ client.c
@@ -1,12 +1,6 @@
 #include "snakeio.h"
 
 int main(){
-	fptr = fopen("log.txt","w");
-	if (fptr == NULL){
-		printf("ERROR to open log.txt file.");
-		exit(-1);
-	}
-	fprintf(fptr,"Initialize FILE fptr.\n");
 	Game *game = initGame();
 	startGame(game);
 }
