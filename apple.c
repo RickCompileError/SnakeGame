83a28679634f120a1b59c8af899efc00cc3a37a3
Refrigerator <gngn15401@gmail.com>
2022-06-16 02:09:48 +0800

    fix apple position bug

--- apple.c
+++ apple.c
@@ -1,21 +1,24 @@
 #include "snakeio.h"
 
 void createApple(Game *g){
-     if (g->apple == NULL){
-         int y, x;
-         getEmptyCoordinates(g->board,&y,&x);
-         g->apple = malloc(sizeof(Coordinate));
-         g->apple->y = y;
-         g->apple->x = x;
-         g->apple->ch = 'A';
-         addAt(g->board,*g->apple);
-         fprintf(stderr,"[Apple] Apple generate at Y: %d X: %d\n",gety(*g->apple),getx(*g->apple));
-     }
+    if (g->apple == NULL){
+        int y, x;
+        getEmptyCoordinates(g->board,&y,&x);
+        g->apple = malloc(sizeof(Coordinate));
+        g->apple->y = y;
+        g->apple->x = x;
+        g->apple->ch = 'A';
+        addAt(g->board,*g->apple);
+        fprintf(stderr,"[Apple] Apple generate at Y: %d X: %d\n",gety(*g->apple),getx(*g->apple));
+    }
 }
 
 void addApple(Game *g, Coordinate new_apple){
     if (g->apple == NULL){
-        g->apple = &new_apple;
+        g->apple = malloc(sizeof(Coordinate));
+        g->apple->y = gety(new_apple);
+        g->apple->x = getx(new_apple);
+        g->apple->ch = getchtype(new_apple);
         addAt(g->board, new_apple);
     }
 }
